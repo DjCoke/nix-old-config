9ba8b6aba111ddad3e4bcef6086cc31e8861b641
DjCoke <erwin.vd.glind@me.com>
2024-11-17 20:33:51 +0100

    Changed the bat.nix module, and added a TODO

--- home/modules/bat.nix
+++ home/modules/bat.nix
@@ -1,5 +1,16 @@
-{...}: {
+{ ... }: {
   # Install bat via home-manager module
+
+  # TODO: see if we need to add this?
+
+  # home.file.".config/bat/config".text = ''
+  #    --theme="Dracula"
+  #
+  #    # Show line numbers, Git modifications and file header (but no grid)
+  #    --style="numbers,changes,header"
+  #  '';
+
+
   programs.bat = {
     enable = true;
     catppuccin.enable = true;
