7f35178ac1b42445b054098408c0d2e80843aff9
DjCoke <erwin.vd.glind@me.com>
2024-11-17 16:33:34 +0100

    Remove spicetefy from config (flake.nix)

--- flake.nix
+++ flake.nix
@@ -17,11 +17,7 @@
     # Global catppuccin theme
     catppuccin.url = "github:catppuccin/nix";
 
-    # NixOS Spicetify
-    spicetify-nix = {
-      url = "github:Gerg-L/spicetify-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # Removed spicetefy (because I do not use it)
 
     # Nix Darwin (for MacOS machines)
     darwin = {
@@ -33,82 +29,84 @@
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
-  outputs = {
-    self,
-    catppuccin,
-    darwin,
-    home-manager,
-    nix-homebrew,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
+  outputs =
+    { self
+    , catppuccin
+    , darwin
+    , home-manager
+    , nix-homebrew
+    , nixpkgs
+    , ...
+    } @ inputs:
+    let
+      inherit (self) outputs;
 
-    # Define user configurations
-    users = {
-      nabokikh = {
-        avatar = ./files/avatar/face;
-        email = "alexander.nabokikh@olx.pl";
-        fullName = "Alexander Nabokikh";
-        gitKey = "C5810093";
-        name = "nabokikh";
-      };
-    };
-
-    # Function for NixOS system configuration
-    mkNixosConfiguration = hostname: username:
-      nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit inputs outputs hostname;
-          userConfig = users.${username};
+      # Define user configurations
+      users = {
+        nabokikh = {
+          avatar = ./files/avatar/face;
+          email = "alexander.nabokikh@olx.pl";
+          fullName = "Alexander Nabokikh";
+          gitKey = "C5810093";
+          name = "nabokikh";
         };
-        modules = [./hosts/${hostname}/configuration.nix];
       };
 
-    # Function for nix-darwin system configuration
-    mkDarwinConfiguration = hostname: username:
-      darwin.lib.darwinSystem {
-        system = "aarch64-darwin";
-        specialArgs = {
-          inherit inputs outputs hostname;
-          userConfig = users.${username};
+      # Function for NixOS system configuration
+      mkNixosConfiguration = hostname: username:
+        nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs outputs hostname;
+            userConfig = users.${username};
+          };
+          modules = [ ./hosts/${hostname}/configuration.nix ];
         };
-        modules = [
-          ./hosts/${hostname}/configuration.nix
-          home-manager.darwinModules.home-manager
-          nix-homebrew.darwinModules.nix-homebrew
-        ];
-      };
 
-    # Function for Home Manager configuration
-    mkHomeConfiguration = system: username: hostname:
-      home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {inherit system;};
-        extraSpecialArgs = {
-          inherit inputs outputs;
-          userConfig = users.${username};
+      # Function for nix-darwin system configuration
+      mkDarwinConfiguration = hostname: username:
+        darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          specialArgs = {
+            inherit inputs outputs hostname;
+            userConfig = users.${username};
+          };
+          modules = [
+            ./hosts/${hostname}/configuration.nix
+            home-manager.darwinModules.home-manager
+            nix-homebrew.darwinModules.nix-homebrew
+          ];
         };
-        modules = [
-          ./home/${username}/${hostname}.nix
-          catppuccin.homeManagerModules.catppuccin
-        ];
+
+      # Function for Home Manager configuration
+      mkHomeConfiguration = system: username: hostname:
+        home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs { inherit system; };
+          extraSpecialArgs = {
+            inherit inputs outputs;
+            userConfig = users.${username};
+          };
+          modules = [
+            ./home/${username}/${hostname}.nix
+            catppuccin.homeManagerModules.catppuccin
+          ];
+        };
+    in
+    {
+      nixosConfigurations = {
+        energy = mkNixosConfiguration "energy" "nabokikh";
+        nabokikh-z13 = mkNixosConfiguration "nabokikh-z13" "nabokikh";
       };
-  in {
-    nixosConfigurations = {
-      energy = mkNixosConfiguration "energy" "nabokikh";
-      nabokikh-z13 = mkNixosConfiguration "nabokikh-z13" "nabokikh";
-    };
 
-    darwinConfigurations = {
-      "nabokikh-mac" = mkDarwinConfiguration "nabokikh-mac" "nabokikh";
-    };
+      darwinConfigurations = {
+        "nabokikh-mac" = mkDarwinConfiguration "nabokikh-mac" "nabokikh";
+      };
 
-    homeConfigurations = {
-      "nabokikh@energy" = mkHomeConfiguration "x86_64-linux" "nabokikh" "energy";
-      "nabokikh@nabokikh-mac" = mkHomeConfiguration "aarch64-darwin" "nabokikh" "nabokikh-mac";
-      "nabokikh@nabokikh-z13" = mkHomeConfiguration "x86_64-linux" "nabokikh" "nabokikh-z13";
-    };
+      homeConfigurations = {
+        "nabokikh@energy" = mkHomeConfiguration "x86_64-linux" "nabokikh" "energy";
+        "nabokikh@nabokikh-mac" = mkHomeConfiguration "aarch64-darwin" "nabokikh" "nabokikh-mac";
+        "nabokikh@nabokikh-z13" = mkHomeConfiguration "x86_64-linux" "nabokikh" "nabokikh-z13";
+      };
 
-    overlays = import ./overlays {inherit inputs;};
-  };
+      overlays = import ./overlays { inherit inputs; };
+    };
 }
