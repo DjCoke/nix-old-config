87f299cdcc19aaeda6eb0b3f70bdbdb374793411
DjCoke <erwin.vd.glind@me.com>
2024-11-18 22:44:43 +0100

    Added more sops config and secrets.yaml to the repo

--- hosts/modules/sops.nix
+++ hosts/modules/sops.nix
@@ -3,4 +3,27 @@
   imports = [
     inputs.sops-nix.nixosModules.sops
   ];
+
+  sops = {
+    
+    defaultSopsFile = ../../secrets.yaml
+    validateSopsFiles = false;
+
+    age = {
+      # automatically import host SSH keys as ages keys
+      sshKeyPaths = [ "/ets/ssh/ssh_host_ed25519_key" ];
+      # this will use an age key that is expected to already be in the filesystem
+      keyFile = "/var/lib/sops-nix/key.txt";
+      # generate a new key if the key specified above does not exist
+      generateKey = true;
+    };
+
+    # secrets will be output to /run/secrets
+    # e.g. /run/secrets/msmtp-password
+    # secrets required for use creation are handled in respective ./home/<username>.nix files
+    # because they will be output to /run/secrets/-for-users and only when the user is assigned to host
+    secrets = {
+      msmtp-password = {};
+    };
+  };
 }
