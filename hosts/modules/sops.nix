14f97ffc2e8f41f6676b44b3272a3814b0ad12c5
DjCoke <erwin.vd.glind@me.com>
2024-11-18 22:48:03 +0100

    Typo

--- hosts/modules/sops.nix
+++ hosts/modules/sops.nix
@@ -11,7 +11,7 @@
 
     age = {
       # automatically import host SSH keys as ages keys
-      sshKeyPaths = [ "/ets/ssh/ssh_host_ed25519_key" ];
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       # this will use an age key that is expected to already be in the filesystem
       keyFile = "/var/lib/sops-nix/key.txt";
       # generate a new key if the key specified above does not exist
