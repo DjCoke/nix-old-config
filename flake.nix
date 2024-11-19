410f36cf42a2ec52cf040c40feafb5c46878c344
DjCoke <erwin.vd.glind@me.com>
2024-11-19 23:31:36 +0100

    typo

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
 
     # Private secrets repo.  See https://github.com/EmergentMind/nix-config/blob/dev/docs/secretsmgmt.md
     # Authenticate via ssh and use shallow clone
-    nix-secrets = {
+    mysecrets = {
       url = "git+ssh://git@github.com/DjCoke/nix-secrets.git?ref=main&shallow=1";
       flake = false;
     };
