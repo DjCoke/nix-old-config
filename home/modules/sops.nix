862769e092aceaebda2c009390feb0dd2936cad8
DjCoke <erwin.vd.glind@me.com>
2024-11-19 21:36:31 +0100

    Added alle necessary files for sops

--- home/modules/sops.nix
+++ home/modules/sops.nix
@@ -1,11 +1,22 @@
 # sops configuration for Home Manager
 
-{ inputs, ... }:
+{ inputs, userConfig, ... }:
 {
   imports = [
     inputs.sops-nix.homeManagerModules.sops
   ];
 
+  sops = {
+    #this is the erwin/dev key and need to have been copied to this location on the host
+    age.keyFile = "/home/erwin/.config/sops/age/keys.txt";
+  };
 
+  defaultSopsFile = ../../secrets.yaml;
+  validateSopsFiles = false;
 
+  secrets = {
+    "private_keys/${userConfig.name}" = {
+      path = "/home/${userConfig.name}/.ssh/id_ed25519";
+    };
+  };
 }
