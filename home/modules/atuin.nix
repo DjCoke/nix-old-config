fbbcfc9a7e5f2d8dd625d4a4b63014ed99c3270b
DjCoke <erwin.vd.glind@me.com>
2024-11-19 22:45:03 +0100

    Some changes to atuin

--- home/modules/atuin.nix
+++ home/modules/atuin.nix
@@ -11,9 +11,22 @@
       inline_height = 25;
       invert = true;
       records = true;
-      search_mode = "skim";
+      search_mode = "fuzzy";
+      filter_mode_shell_up_key_binding = "session";
+      filter_mode = "global";
+      enter_accept = true;
+      #TODO:(atuin) disable when comfortable
+      show_help = true;
+      prefers_reduced_motion = true;
       secrets_filter = true;
       style = "compact";
+
+      # This came from https://github.com/nifoc/dotfiles/blob/ce5f9e935db1524d008f97e04c50cfdb41317766/home/programs/atuin.nix#L2
+      history_filter = [
+        "^base64decode"
+        "^instagram-dl"
+        "^mp4concat"
+      ];
     };
     # We use down to trigger, and use up to quickly edit the last entry only
     flags = [ "--disable-up-arrow" ];
