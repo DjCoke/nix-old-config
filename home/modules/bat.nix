801f4513f5434f1bbe054bedd64492abac33a6e0
DjCoke <erwin.vd.glind@me.com>
2024-11-19 21:24:42 +0100

    Some small changes to bat

--- home/modules/bat.nix
+++ home/modules/bat.nix
@@ -1,18 +1,13 @@
 { ... }: {
   # Install bat via home-manager module
 
-  # TODO: see if we need to add this?
-
-  # home.file.".config/bat/config".text = ''
-  #    --theme="Dracula"
-  #
-  #    # Show line numbers, Git modifications and file header (but no grid)
-  #    --style="numbers,changes,header"
-  #  '';
-
-
   programs.bat = {
     enable = true;
     catppuccin.enable = true;
+    config = {
+      pager = "less -FR";
+      style = "numbers,changes,header";
+    };
   };
 }
+
