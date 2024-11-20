38e67c4430aa198fa6ac1f47755a1a273e90a9fc
DjCoke <erwin.vd.glind@me.com>
2024-11-20 22:18:09 +0100

    Commented out, as disko will handle filesystems and partitions

--- hosts/k3s-01/hardware-configuration.nix
+++ hosts/k3s-01/hardware-configuration.nix
@@ -15,19 +15,22 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
-  };
+  # Commented out for disko
+  # fileSystems."/" = {
+  #   device = "/dev/disk/by-label/nixos";
+  #   fsType = "ext4";
+  # };
+  #
+  # fileSystems."/boot" = {
+  #   device = "/dev/disk/by-label/boot";
+  #   fsType = "vfat";
+  #   options = [ "fmask=0022" "dmask=0022" ];
+  # };
+  #
+  # swapDevices =
+  #   [{ device = "/dev/disk/by-label/swap"; }];
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/boot";
-    fsType = "vfat";
-    options = [ "fmask=0022" "dmask=0022" ];
-  };
 
-  swapDevices =
-    [{ device = "/dev/disk/by-label/swap"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
