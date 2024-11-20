50a4811cb6dbcb9682f6e16030cfafdea7511bb3
DjCoke <erwin.vd.glind@me.com>
2024-11-20 23:26:04 +0100

    Added a ZFS disko-config

---
+++ hosts/k3s-01/disko-config.nix
@@ -0,0 +1,54 @@
+{
+  disko.devices = {
+    # disks config
+    disk = {
+      x = {
+        type = "disk";
+        device = "/dev/sda"; # Needs to be named your specific disk's name
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "64M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            zfs = {
+              size = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            };
+          };
+        };
+      };
+    };
+    # zfs zpool config
+    zpool = {
+      zroot = {
+        type = "zpool";
+        rootFsOptions = {
+          compression = "zstd";
+          "com.sun:auto-snapshot" = "false";
+        };
+        mountpoint = "/";
+        postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot@blank$' || zfs snapshot zroot@blank"; # create a blank snapshot on creation
+
+        # datasets config
+        datasets = {
+          zfs_fs = {
+            type = "zfs_fs";
+            mountpoint = "/zfs_fs";
+            options."com.sun:auto-snapshot" = "true";
+          };
+        };
+      };
+    };
+  };
+}
+
