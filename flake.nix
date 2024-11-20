1eb9d4494eeeff66dbc99e62e054c997e41cafcb
DjCoke <erwin.vd.glind@me.com>
2024-11-20 23:56:03 +0100

    Commented out, not yet existing configurations of k3s-{01,02}

--- flake.nix
+++ flake.nix
@@ -119,8 +119,8 @@
     {
       nixosConfigurations = {
         k3s-01 = mkNixosConfiguration "k3s-01" "erwin"; # First k3s-01 node
-        k3s-02 = mkNixosConfiguration "k3s-02" "erwin"; # Second k3s-02 node
-        k3s-03 = mkNixosConfiguration "k3s-03" "erwin"; # Second k3s-03 node
+        #   k3s-02 = mkNixosConfiguration "k3s-02" "erwin"; # Second k3s-02 node
+        #   k3s-03 = mkNixosConfiguration "k3s-03" "erwin"; # Second k3s-03 node
       };
 
       darwinConfigurations = {
