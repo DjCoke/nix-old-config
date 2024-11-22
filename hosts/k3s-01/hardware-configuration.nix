0e366948a678b92f27d548b85ac19eaeba725e74
DjCoke <erwin.vd.glind@me.com>
2024-11-22 14:08:13 +0100

    Trying trying

--- hosts/k3s-01/hardware-configuration.nix
+++ hosts/k3s-01/hardware-configuration.nix
@@ -16,21 +16,21 @@
   boot.extraModulePackages = [ ];
 
   # Commented out for disko
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/boot";
-    fsType = "vfat";
-    options = [ "fmask=0022" "dmask=0022" ];
-  };
-
-  swapDevices =
-    [{ device = "/dev/disk/by-label/swap"; }];
-
-
+  # fileSystems."/" = {
+  #   device = "/dev/disk/by-label/nixos";
+  #   fsType = "ext4";
+  # };
+  #
+  # fileSystems."/boot" = {
+  #   device = "/dev/disk/by-label/boot";
+  #   fsType = "vfat";
+  #   options = [ "fmask=0022" "dmask=0022" ];
+  # };
+  #
+  # swapDevices =
+  #   [{ device = "/dev/disk/by-label/swap"; }];
+  #
+  #
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
